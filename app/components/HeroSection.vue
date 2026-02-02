5a155d1452c5052fdcb14b59d95ec0789563c9d3
Oluwasegun Adedeji <oluwasheges@gmail.com>
2026-02-02 10:48:50 +0100

    Update HeroSection.vue

--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -15,27 +15,52 @@
               grow your startup business quickly
             </p>
 
-            <div class="mt-8">
-              <button
-                class="bg-blue-600 text-white px-6 py-3 rounded-md mr-4 hover:bg-blue-700 transition"
+            <div class="mt-8 flex items-center justify-start gap-7.5 flex-wrap">
+              <a
+                class="bg-olive-medium text-white px-6 py-3 rounded-[100px] h-15 w-55 mr-4 hover:bg-olive flex items-center justify-center duration-300 transition group"
+                href="#"
               >
-                Get Started
-              </button>
-              <button
-                class="bg-gray-200 text-gray-800 px-6 py-3 rounded-md hover:bg-gray-300 transition"
+                <span
+                  class="relative h-7 w-32 overflow-hidden flex items-center justify-center *:font-bold"
+                >
+                  <!-- Initial text (slides up and out on hover) -->
+                  <span
+                    class="absolute w-full transition-all text-center duration-300 ease-in-out group-hover:-translate-y-8 group-hover:opacity-0"
+                  >
+                    Get Started
+                  </span>
+
+                  <!-- Hidden text (slides up from bottom on hover) -->
+                  <span
+                    class="absolute w-full text-black text-center transition-all duration-300 ease-in-out translate-y-8 opacity-0 group-hover:translate-y-0 group-hover:opacity-100"
+                  >
+                    Get Started
+                  </span>
+                </span>
+              </a>
+              <a
+                class="text-white px-6 py-3 bg-transparent group font-bold flex gap-3.5 items-center transition duration-300 hover:text-olive-medium"
+                href="#"
               >
-                Learn More
-              </button>
+                Let's Talk
+                <span class="bg-white/20 group-hover:bg-olive-medium rounded-full flex items-center justify-center transition duration-300 h-7.5 w-7.5">
+                  <UIcon name="lucide:chevron-right" class="text-white  transition duration-300 group-hover:-rotate-45" />
+                </span>
+              </a>
             </div>
           </div>
         </div>
         <div class="ml-auto max-w-118 flex items-center justify-end relative">
           <div class="h-85 relative overflow-hidden rounded-4xl">
             <video class="h-full w-full object-cover" loop autoplay playsinline>
-              <source src="https://staco-react.vercel.app/assets/h6-video-DaLtBHE1.mp4">
+              <source
+                src="https://staco-react.vercel.app/assets/h6-video-DaLtBHE1.mp4"
+              />
             </video>
-            <button class="absolute h-12.5 w-12.5 right-7.5 bottom-7.5 bg-white flex items-center justify-center text-olive-medium rounded-full">
-              <UIcon name="material-symbols-light:pause" class="w-6 h-6"/>
+            <button
+              class="absolute h-12.5 w-12.5 right-7.5 bottom-7.5 bg-white flex items-center justify-center text-olive-medium rounded-full"
+            >
+              <UIcon name="material-symbols-light:pause" class="w-6 h-6" />
             </button>
           </div>
         </div>
